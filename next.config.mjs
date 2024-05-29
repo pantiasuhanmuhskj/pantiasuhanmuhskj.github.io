a4291bcb869aa7adf7fc0ebfe4cbe71d839d3cef
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2024-05-29 08:48:28 +0700

    commit setelah pull github pages nextjs.yml

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
 
-export default nextConfig;
+// export default nextConfig;
+
+//* =============================================
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  // Optional: Change the output directory `out` -> `dist`
+  distDir: "dist",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
