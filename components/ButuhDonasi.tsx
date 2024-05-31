8226ff1eddce478babef47128f7f6ebf6ec65638
Yanuar Lukman Arif <yanuarlukmanarif@gmail.com>
2024-05-31 09:08:58 +0700

    update tampilan butuh donasi, tambah countup

--- components/ButuhDonasi.tsx
+++ components/ButuhDonasi.tsx
@@ -1,15 +1,65 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
+import CountUp from "react-countup";
+import { GiReceiveMoney, GiMoneyStack } from "react-icons/gi";
 
 const ButuhDonasi = () => {
   return (
-    <div className="w-full bg-green-200 py-10">
+    <div className="w-full bg-green-100 py-10 text-gray-900">
       <div className="flex flex-col items-center justify-center">
         <h1>Sedang Membutuhkan Donasi Segera...</h1>
-        <p>
+        {/* <p>
           Senilai <span className="font-bold">Rp 737.910.000</span>
-        </p>
-        <p className="mt-5">
+        </p> */}
+        <div className="flex flex-col md:flex-row gap-5 md:gap-20 my-5 items-center">
+          {/* Total Donasi Dibutuhkan */}
+          <div>
+            <div className="flex flex-col items-center gap-3">
+              <div>
+                <p className="text-xl font-semibold">Total Donasi Dibutuhkan</p>
+              </div>
+              <div className="flex items-end justify-center">
+                <GiReceiveMoney size={40} className="mr-5" color="#00796B" />
+                <div className="flex font-semibold gap-1 text-lg">
+                  <p>Rp. </p>
+                  <CountUp
+                    end={737910000}
+                    duration={10}
+                    formattingFn={(value) =>
+                      value.toLocaleString("id-ID", {
+                        minimumFractionDigits: 2,
+                      })
+                    }
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Total Donasi Terkumpul */}
+          <div>
+            <div className="flex flex-col items-center gap-3">
+              <div>
+                <p className="text-xl font-semibold">Total Donasi Terkumpul</p>
+              </div>
+              <div className="flex items-end justify-center">
+                <GiMoneyStack size={40} className="mr-5" color="#00796B" />
+                <div className="flex font-semibold gap-1 text-lg">
+                  <CountUp
+                    end={100955000}
+                    duration={10}
+                    formattingFn={(value) =>
+                      value.toLocaleString("id-ID", {
+                        minimumFractionDigits: 2,
+                      })
+                    }
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <p className="">
           Info donasi lebih lengkap klik{" "}
           <Link href="/programdonasi" className="underline hover:text-blue-300">
             Program Donasi
